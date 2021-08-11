d65dd8ddc41cdcffd375952d60335a7d1b9ae08b
luqmanishere <luqmanulhakim1720@gmail.com>
2021-08-11 23:21:35 +0000

    Edited the description

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
-  description = "A very basic flake";
+  description = "Fixes vscode-server on NixOS";
 
   outputs = { self, nixpkgs }: {
 
+    # Export the module under the name vscode
     nixosModules.vscode = import ./modules/vscode-server;
     nixosModule = self.nixosModules.vscode;
   };
